56a2db209fa5ef3ee4306775950231007b2d4a98
mchrh <othmachrouh@gmail.com>
2020-02-22 21:22:31 +0100

    initial search ready for prod

---
+++ webapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class results(models.Model):
+    id=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=100)
+    ING1=models.CharField(max_length=100)
+    ING2 = models.CharField(max_length=100)
+    ING3 = models.CharField(max_length=100)
+    ING4 = models.CharField(max_length=100)
+    ING5 = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural="recipes"
+
+    def __str__(self):
+        return self.name
+
+
+
---
+++ webapp/views.py
@@ -0,0 +1,18 @@
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
+from .models import results
+
+class HomePageView(TemplateView):
+    template_name='index.html'
+
+class SearchResultsView(ListView):
+    model=results
+    template_name='search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = results.objects.filter(
+            Q(name__icontains=query) | Q(ING1__icontains=query)
+        )
+        return object_list
+
