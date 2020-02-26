c078e1687a144e283dadeeb2c3e99318dc59206c
mchrh <othmachrouh@gmail.com>
2020-02-26 22:10:03 +0100

    tweaked search query

--- webapp/views.py
+++ webapp/views.py
@@ -12,7 +12,7 @@ class SearchResultsView(ListView):
     def get_queryset(self):
         query = self.request.GET.get('q')
         object_list = results.objects.filter(
-            Q(name__icontains=query) | Q(ING1__icontains=query)
+            Q(name__icontains=query) | Q(ING1__icontains=query) | Q(ING2__icontains=query) | Q(ING3__icontains=query) | Q(ING4__icontains=query) | Q(ING5__icontains=query)
         )
         return object_list
 
