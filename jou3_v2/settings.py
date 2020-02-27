379f0ca870c0eef29621fa9769f4722f090cf75d
mchrh <othmachrouh@gmail.com>
2020-02-27 15:11:56 +0100

    turned off debug mode

--- jou3_v2/settings.py
+++ jou3_v2/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*k1&1*p)^35pd7djqe4zdh-%a3*z@a8$bqm^kpnek(7^r_i(2+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '.mchrh.me']
 
